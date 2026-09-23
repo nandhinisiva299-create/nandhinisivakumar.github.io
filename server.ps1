4b979429e64014c5aabb30be5d87339c18500c25
nandhinisiva299-create <nandhinisiva299@gmail.com>
2026-09-23 10:26:43 +0530

    Add/Update server.ps1 (E-commerce Animation Suite)

---
+++ server.ps1
@@ -0,0 +1,51 @@
+$port = 8080
+$prefix = "http://localhost:$port/"
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add($prefix)
+$listener.Start()
+Write-Host "Server started at $prefix"
+
+$baseDir = "c:\Users\Nandhini\OneDrive\Documents\ecommerce-animation-showcase"
+
+$mimeTypes = @{
+    ".html" = "text/html; charset=utf-8"
+    ".css"  = "text/css; charset=utf-8"
+    ".js"   = "application/javascript; charset=utf-8"
+    ".json" = "application/json; charset=utf-8"
+    ".png"  = "image/png"
+    ".jpg"  = "image/jpeg"
+    ".svg"  = "image/svg+xml"
+    ".woff2"= "font/woff2"
+}
+
+try {
+    while ($listener.IsListening) {
+        $context = $listener.GetContext()
+        $request = $context.Request
+        $response = $context.Response
+
+        $rawUrl = $request.Url.LocalPath
+        if ($rawUrl -eq "/" -or $rawUrl -eq "") {
+            $rawUrl = "/index.html"
+        }
+
+        $filePath = Join-Path $baseDir $rawUrl.TrimStart('/')
+        if (Test-Path $filePath -PathType Leaf) {
+            $ext = [System.IO.Path]::GetExtension($filePath).ToLower()
+            $contentType = if ($mimeTypes.ContainsKey($ext)) { $mimeTypes[$ext] } else { "application/octet-stream" }
+            $response.ContentType = $contentType
+            $bytes = [System.IO.File]::ReadAllBytes($filePath)
+            $response.ContentLength64 = $bytes.Length
+            $response.OutputStream.Write($bytes, 0, $bytes.Length)
+        } else {
+            $response.StatusCode = 404
+            $msg = [System.Text.Encoding]::UTF8.GetBytes("404 Not Found")
+            $response.ContentLength64 = $msg.Length
+            $response.OutputStream.Write($msg, 0, $msg.Length)
+        }
+        $response.Close()
+    }
+} finally {
+    $listener.Stop()
+    $listener.Close()
+}
